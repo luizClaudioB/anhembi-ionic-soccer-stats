5118aa90a81e27f72294fc5ffab86306c5cff83b
Raphael <raphaelhcc321@gmail.com>
2020-10-12 19:03:26 -0300

    implementando botões e obtendo dados dos inputs

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public username: string = '';
+  public password: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  logUsername() {
+    console.log('username: ' + this.username)
+  }
+
+  logPassword() {
+    console.log('password: ' + this.password)
   }
 
 }
