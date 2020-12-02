212e39e340dd06fea39be58f352a5e2022304b8c
Leonardo Arruda Mesquita - LMS <leonardo.mesquita@iteris.com.br>
2020-12-02 02:08:24 -0300

    feat: showing current match in tab1 and doing some adjustments

--- src/app/services/football-live/football-live.service.ts
+++ src/app/services/football-live/football-live.service.ts
@@ -11,8 +11,8 @@ export interface clubsImg {
 }
 
 const arrayPaises = {
-    brasil: "2020/br.1.json",
-    inglaterra: "2020-21/en.1.json"
+  brasil: "2020/br.1.json",
+  inglaterra: "2020-21/en.1.json"
 }
 
 const arrayImgEnClubs: Array<clubsImg> = [
@@ -159,144 +159,144 @@ const arrayImgEnClubs: Array<clubsImg> = [
 ]
 const arrayImgBrClubs: Array<clubsImg> = [
   {
-  clube: 'Atlético GO',
-  nome: 'Atlético GO',
-  cidade: 'Goiânia - GO',
-  url: "https://a1.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F10357.png",
-  estadio: 'Serra Dourada'
+    clube: 'Atlético GO',
+    nome: 'Atlético GO',
+    cidade: 'Goiânia - GO',
+    url: "https://a1.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F10357.png",
+    estadio: 'Serra Dourada'
   },
   {
-  clube: 'Atlético PR',
-  nome: 'Atlético PR',
-  cidade: 'Curitiba - PR',
-  url: "https://a.espncdn.com/i/teamlogos/soccer/500/3458.png",
-  estadio: 'Arena da Baixada'
+    clube: 'Atlético PR',
+    nome: 'Atlético PR',
+    cidade: 'Curitiba - PR',
+    url: "https://a.espncdn.com/i/teamlogos/soccer/500/3458.png",
+    estadio: 'Arena da Baixada'
   },
   {
-  clube: 'EC Bahia',
-  nome: 'Bahia',
-  cidade: 'Salvador - BA',
-  url: "https://www.esporteclubebahia.com.br/wp-content/themes/2016/img/main/logo-bahia-max.png",
-  estadio: 'Fonte Nova'
+    clube: 'EC Bahia',
+    nome: 'Bahia',
+    cidade: 'Salvador - BA',
+    url: "https://www.esporteclubebahia.com.br/wp-content/themes/2016/img/main/logo-bahia-max.png",
+    estadio: 'Fonte Nova'
   },
   {
-  clube: 'RB Bragantino',
-  nome: 'RB Bragantino',
-  cidade: 'Bragança Paulista - SP',
-  url: "https://upload.wikimedia.org/wikipedia/pt/9/9e/RedBullBragantino.png",
-  estadio: 'Nabi Abi Chedid'
+    clube: 'RB Bragantino',
+    nome: 'RB Bragantino',
+    cidade: 'Bragança Paulista - SP',
+    url: "https://upload.wikimedia.org/wikipedia/pt/9/9e/RedBullBragantino.png",
+    estadio: 'Nabi Abi Chedid'
   },
   {
-  clube: 'Botafogo RJ',
-  nome: 'Botafogo',
-  cidade: 'Rio de Janeiro - RJ',
-  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Botafogo_de_Futebol_e_Regatas_logo.svg/1200px-Botafogo_de_Futebol_e_Regatas_logo.svg.png",
-  estadio: 'Engenhão'
+    clube: 'Botafogo RJ',
+    nome: 'Botafogo',
+    cidade: 'Rio de Janeiro - RJ',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Botafogo_de_Futebol_e_Regatas_logo.svg/1200px-Botafogo_de_Futebol_e_Regatas_logo.svg.png",
+    estadio: 'Engenhão'
   },
   {
-  clube: 'Coritiba PR',
-  nome: 'Coritiba',
-  cidade: 'Curitiba - PR',
-  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Coritiba_FBC_%282011%29_-_PR.svg/1200px-Coritiba_FBC_%282011%29_-_PR.svg.png",
-  estadio: 'Couto Pereira'
+    clube: 'Coritiba PR',
+    nome: 'Coritiba',
+    cidade: 'Curitiba - PR',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Coritiba_FBC_%282011%29_-_PR.svg/1200px-Coritiba_FBC_%282011%29_-_PR.svg.png",
+    estadio: 'Couto Pereira'
   },
   {
-  clube: 'Ceára SC',
-  nome: 'Ceará',
-  cidade: 'Fortaleza - CE',
-  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Cear%C3%A1_Sporting_Club_logo.svg/1200px-Cear%C3%A1_Sporting_Club_logo.svg.png",
-  estadio: 'Castelão'
+    clube: 'Ceára SC',
+    nome: 'Ceará',
+    cidade: 'Fortaleza - CE',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Cear%C3%A1_Sporting_Club_logo.svg/1200px-Cear%C3%A1_Sporting_Club_logo.svg.png",
+    estadio: 'Castelão'
   },
   {
-  clube: 'Corinthians SP',
-  nome: 'Corinthians',
-  cidade: 'São Paulo - SP',
-  url: "https://a3.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F874.png",
-  estadio: 'Arena Neo Quimica'
+    clube: 'Corinthians SP',
+    nome: 'Corinthians',
+    cidade: 'São Paulo - SP',
+    url: "https://a3.espncdn.com/combiner/i?img=%2Fi%2Fteamlogos%2Fsoccer%2F500%2F874.png",
+    estadio: 'Arena Neo Quimica'
   },
   {
-  clube: 'Goiás EC',
-  nome: 'Goiás',
-  cidade: 'Goiânia - GO',
-  url: "https://upload.wikimedia.org/wikipedia/pt/3/38/GoiasEC2019.png",
-  estadio: 'Serra Dourada'
+    clube: 'Goiás EC',
+    nome: 'Goiás',
+    cidade: 'Goiânia - GO',
+    url: "https://upload.wikimedia.org/wikipedia/pt/3/38/GoiasEC2019.png",
+    estadio: 'Serra Dourada'
   },
   {
-  clube: 'Flamengo RJ',
-  nome: 'Flamengo',
-  cidade: 'Rio de Janeiro - RJ',
-  url: "https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png" ,
-  estadio: 'Maracanã'
+    clube: 'Flamengo RJ',
+    nome: 'Flamengo',
+    cidade: 'Rio de Janeiro - RJ',
+    url: "https://upload.wikimedia.org/wikipedia/commons/9/93/Flamengo-RJ_%28BRA%29.png",
+    estadio: 'Maracanã'
   },
   {
-  clube: 'Santos SP',
-  nome: 'Santos',
-  cidade: 'Santos - SP',
-  url: "https://www.santosfc.com.br/wp-content/themes/santosfc-theme/assets/images/logo-santos.png",
-  estadio: 'Vila Belmiro'
+    clube: 'Santos SP',
+    nome: 'Santos',
+    cidade: 'Santos - SP',
+    url: "https://www.santosfc.com.br/wp-content/themes/santosfc-theme/assets/images/logo-santos.png",
+    estadio: 'Vila Belmiro'
   },
   {
-  clube: 'Sport Recife',
-  nome: 'Sport',
-  cidade: 'Recife - PE',
-  url: "https://upload.wikimedia.org/wikipedia/pt/1/17/Sport_Club_do_Recife.png",
-  estadio: 'Ilha do Retiro'
+    clube: 'Sport Recife',
+    nome: 'Sport',
+    cidade: 'Recife - PE',
+    url: "https://upload.wikimedia.org/wikipedia/pt/1/17/Sport_Club_do_Recife.png",
+    estadio: 'Ilha do Retiro'
   },
   {
-  clube: 'Palmeiras SP', 
-  nome: 'Palmeiras',
-  cidade: 'São Paulo - SP',
-  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/200px-Palmeiras_logo.svg.png",
-  estadio: 'Allianz Parque'
+    clube: 'Palmeiras SP',
+    nome: 'Palmeiras',
+    cidade: 'São Paulo - SP',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/200px-Palmeiras_logo.svg.png",
+    estadio: 'Allianz Parque'
   },
   {
-  clube: 'São Paulo FC',
-  nome: 'São Paulo',
-  cidade: 'São Paulo - SP',
-  url: "https://upload.wikimedia.org/wikipedia/pt/4/4b/S%C3%A3o_Paulo_Futebol_Clube.png",
-  estadio: 'Morumbi'
+    clube: 'São Paulo FC',
+    nome: 'São Paulo',
+    cidade: 'São Paulo - SP',
+    url: "https://upload.wikimedia.org/wikipedia/pt/4/4b/S%C3%A3o_Paulo_Futebol_Clube.png",
+    estadio: 'Morumbi'
   },
   {
-  clube: 'Internacional Porto Alegre',
-  nome: 'Internacional',
-  cidade: 'Porto Alegre - RS',
-  url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Escudo_do_Sport_Club_Internacional.svg/1200px-Escudo_do_Sport_Club_Internacional.svg.png",
-  estadio: 'Beira-Rio'
+    clube: 'Internacional Porto Alegre',
+    nome: 'Internacional',
+    cidade: 'Porto Alegre - RS',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Escudo_do_Sport_Club_Internacional.svg/1200px-Escudo_do_Sport_Club_Internacional.svg.png",
+    estadio: 'Beira-Rio'
   },
   {
-  clube: 'CR Vasco da Gama',
-  nome: 'Vasco',
-  cidade: 'Rio de Janeiro - RJ',
-  url: "https://upload.wikimedia.org/wikipedia/pt/8/89/Club_de_Regatas_Vasco_da_Gama.png",
-  estadio: 'São Januário'
+    clube: 'CR Vasco da Gama',
+    nome: 'Vasco',
+    cidade: 'Rio de Janeiro - RJ',
+    url: "https://upload.wikimedia.org/wikipedia/pt/8/89/Club_de_Regatas_Vasco_da_Gama.png",
+    estadio: 'São Januário'
   },
   {
-  clube: 'Grêmio RS',
-  nome: 'Grêmio',
-  cidade: 'Porto Alegre - RS',
-  url: "https://upload.wikimedia.org/wikipedia/pt/a/a1/Gremio.png",
-  estadio: 'Arena do Grêmio'
+    clube: 'Grêmio RS',
+    nome: 'Grêmio',
+    cidade: 'Porto Alegre - RS',
+    url: "https://upload.wikimedia.org/wikipedia/pt/a/a1/Gremio.png",
+    estadio: 'Arena do Grêmio'
   },
   {
-  clube: 'Fortaleza EC',
-  nome: 'Fortaleza',
-  cidade: 'Fortaleza - CE',
-  url: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Escudo_do_Fortaleza_EC.png",
-  estadio: 'Castelão'
+    clube: 'Fortaleza EC',
+    nome: 'Fortaleza',
+    cidade: 'Fortaleza - CE',
+    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/FortalezaEsporteClube.svg/180px-FortalezaEsporteClube.svg.png",
+    estadio: 'Castelão'
   },
   {
-  clube: 'Fluminense RJ',
-  nome: 'Fluminense',
-  cidade: 'Rio de Janeiro - RJ',
-  url: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Fluminense_FC_escudo.png",
-  estadio: 'Maracanã'
+    clube: 'Fluminense RJ',
+    nome: 'Fluminense',
+    cidade: 'Rio de Janeiro - RJ',
+    url: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Fluminense_FC_escudo.png",
+    estadio: 'Maracanã'
   },
   {
-  clube: 'Atlético MG',
-  nome: 'Atlético MG',
-  cidade: 'Belo Horizonte - MG',
-  url: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Atletico_mineiro_galo.png",
-  estadio: 'Mineirão'
+    clube: 'Atlético MG',
+    nome: 'Atlético MG',
+    cidade: 'Belo Horizonte - MG',
+    url: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Atletico_mineiro_galo.png",
+    estadio: 'Mineirão'
   }
 ]
 const arrayClubesPaises = {
@@ -309,49 +309,51 @@ const arrayClubesPaises = {
 })
 export default class FootballLiveService {
   constructor(private connectionService: ConnectionService) { }
-  
+
   getChampionship(país) {
-      let teamsImg1: any = '';
-      let teamsImg2: any = '';
-      let SELECTED_COUNTRY: string = null;
+    let teamsImg1: any = '';
+    let teamsImg2: any = '';
+    let SELECTED_COUNTRY: string = null;
 
-      país === 'brasil' ? SELECTED_COUNTRY = arrayPaises.brasil : SELECTED_COUNTRY = arrayPaises.inglaterra
+    país === 'brasil' ? SELECTED_COUNTRY = arrayPaises.brasil : SELECTED_COUNTRY = arrayPaises.inglaterra
 
-      return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
       const query = "https://raw.githubusercontent.com/openfootball/football.json/master/" + SELECTED_COUNTRY;
       this.connectionService.executeGet(query)
         .subscribe((result: MatchesModel) => {
-            for(let i = 0; i < result.matches.length; i++){
-            país === 'brasil' 
+          for (let i = 0; i < result.matches.length; i++) {
+            país === 'brasil'
               ? (teamsImg1 = arrayImgBrClubs.filter((club) => club.clube === result.matches[i].team1),
                 teamsImg2 = arrayImgBrClubs.filter((club) => club.clube === result.matches[i].team2))
               : (teamsImg1 = arrayImgEnClubs.filter((club) => club.clube === result.matches[i].team1),
                 teamsImg2 = arrayImgEnClubs.filter((club) => club.clube === result.matches[i].team2));
 
-            result.matches[i] = Object.assign(result.matches[i], { team1Img: teamsImg1[0].url, 
-              team2Img: teamsImg2[0].url, stadium: teamsImg1[0].stadium})
-            }
-            resolve(result);
+            result.matches[i] = Object.assign(result.matches[i], {
+              team1Img: teamsImg1[0].url,
+              team2Img: teamsImg2[0].url, stadium: teamsImg1[0].stadium
+            })
+          }
+          resolve(result);
         });
-      });
-    }
+    });
+  }
 
   getClubsFromChampionship(country) {
     let SELECTED_COUNTRY: string = null;
     let clubStadium: any = null;
-    country === 'brasil' 
-      ? SELECTED_COUNTRY = arrayClubesPaises.brasil 
+    country === 'brasil'
+      ? SELECTED_COUNTRY = arrayClubesPaises.brasil
       : SELECTED_COUNTRY = arrayClubesPaises.inglaterra
 
     return new Promise((resolve, reject) => {
       const query = "https://raw.githubusercontent.com/openfootball/football.json/master/" + SELECTED_COUNTRY;
       this.connectionService.executeGet(query)
         .subscribe((result: any) => {
-          for(let i = 0; i < result.clubs.length; i++){
+          for (let i = 0; i < result.clubs.length; i++) {
             country === 'brasil' ? clubStadium = arrayImgBrClubs.filter((club) => club.clube === result.clubs[i].name)
               : clubStadium = arrayImgEnClubs.filter((club) => club.clube === result.clubs[i].name)
 
-            result.clubs[i] = Object.assign(result.clubs[i], 
+            result.clubs[i] = Object.assign(result.clubs[i],
               { stadium: clubStadium[0].estadio, url: clubStadium[0].url, clubName: clubStadium[0].nome, city: clubStadium[0].cidade })
           }
           resolve(result);
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,87 +1,98 @@
-import { Component, Injectable, OnInit, OnChanges, ɵɵstylePropInterpolate1 } from '@angular/core';
+import {
+  Component,
+  Injectable,
+  OnInit
+} from '@angular/core';
 import FootballLiveService from './../services/football-live/football-live.service';
-import { MatchesModel } from '../models/matches.model'
+import { MatchesModel } from '../models/matches.model';
+import * as moment from 'moment';
 
-
-export interface teamsImg {
-  team: string,
-  img: string,
+export interface TeamsImg {
+  team: string;
+  img: string;
 }
-
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-
-
-
 @Injectable()
 export class Tab1Page implements OnInit {
-
-  isLoading: boolean = false;
+  isLoading = false;
   matches: MatchesModel;
-  filterMatches: boolean = false;
-  country: string = 'brasil';
+  filterMatches = false;
+  country = 'brasil';
   standings: Array<any> = null;
   eachRound;
   filteredMatches;
   clubs;
-  RoundData: number= 0;
-  IndexData: number = 0;
+  RoundData = 0;
+  IndexData = 0;
+  nextMatch;
 
+  constructor(private footballLiveService: FootballLiveService) { }
 
-  
-  constructor(
-    private footballLiveService: FootballLiveService,
-  ) {}
-
-  
-  async ngOnInit(){
-    await this.initializeData(this.country)
+  async ngOnInit() {
+    await this.initializeData(this.country);
+    await this.getNextMatch();
   }
 
-  async initializeData(country: string){
+  async initializeData(country: string) {
     await this.getChampionshipData(country);
     await this.getClubsFromChampionship();
   }
 
   public async getChampionshipData(country: string) {
     this.isLoading = true;
-    const result: MatchesModel = await this.footballLiveService.getChampionship(country)
-        this.matches = result;
-        this.getPastRounds();
-        this.getEachRoundSeparated();
-        //this.isLoading = false;
-     
+    const result: MatchesModel = await this.footballLiveService.getChampionship(
+      country
+    );
+    this.matches = result;
+    this.getPastRounds();
+    this.getEachRoundSeparated();
+
     return this.matches;
   }
 
   public async getClubsFromChampionship() {
-    const result: any = await this.footballLiveService.getClubsFromChampionship('brasil')
-      this.clubs = result;
+    const result: any = await this.footballLiveService.getClubsFromChampionship(
+      'brasil'
+    );
+    this.clubs = result;
 
     return this.clubs;
   }
 
-  getPastRounds(){
-    this.filteredMatches = this.matches.matches.filter((placar) => placar?.score?.ft.length > 0);
+  getPastRounds() {
+    this.filteredMatches = this.matches.matches.filter(
+      (placar) => placar?.score?.ft.length > 0
+    );
 
     return this.filteredMatches;
   }
 
-  getEachRoundSeparated(){
-    let newArray = [], rodadasSeparadas;
-    rodadasSeparadas = this.matches.matches.reduce((h, match) => Object.assign(h, { [match.round]:( h[match.round] || [] )
-      .concat({date: match.date, score: match.score, team1: match.team1, team2: match.team2, 
-        teamImg1: match.team1Img, teamImg2: match.team2Img}) }), [{}]);
-    
-    for(let i = 1; i <= 38; i++){
-      let str1 = i.toString();
-      let str2 = this.country === 'brasil' ? 'Rodada' : 'Matchday';
-      let concat = str2 + ' ' + str1;
-      newArray.push(rodadasSeparadas[concat])
+  getEachRoundSeparated() {
+    const newArray = [];
+    const rodadasSeparadas = this.matches.matches.reduce(
+      (h, match) =>
+        Object.assign(h, {
+          [match.round]: (h[match.round] || []).concat({
+            date: match.date,
+            score: match.score,
+            team1: match.team1,
+            team2: match.team2,
+            teamImg1: match.team1Img,
+            teamImg2: match.team2Img,
+          }),
+        }),
+      [{}]
+    );
+
+    for (let i = 1; i <= 38; i++) {
+      const str2 = this.country === 'brasil' ? 'Rodada' : 'Matchday';
+      const concat = `${str2} ${i.toString()}`;
+      newArray.push(rodadasSeparadas[concat]);
     }
     this.eachRound = newArray;
     this.isLoading = false;
@@ -89,17 +100,22 @@ export class Tab1Page implements OnInit {
     return this.eachRound;
   }
 
-  goToSlide(slides){
-    console.log('Round:',this.RoundData + 1)
-    slides.slideTo(this.RoundData)
+  goToSlide(slides) {
+    slides.slideTo(this.RoundData);
   }
 
-  getIndex(slides){
-    slides.getActiveIndex().then(data => {
-      this.IndexData = data; 
-      this.RoundData =0;
-      this.RoundData = this.IndexData 
+  getIndex(slides) {
+    slides.getActiveIndex().then((data) => {
+      this.IndexData = data;
+      this.RoundData = 0;
+      this.RoundData = this.IndexData;
     });
   }
-  }
 
+  getNextMatch() {
+    const currentDate = moment();
+
+    const matchLabel = this.matches.matches.find(match => moment(match.date) > currentDate).round;
+    this.nextMatch = matchLabel.split(' ')[1];
+  }
+}
